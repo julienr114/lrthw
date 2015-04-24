7c47d3547c2309e6ab73911c3a59286bf5d8a745
julienr114 <julienr114@gmail.com>
2015-04-24 18:48:45 +0200

    exercice 10 à 12

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "<Quel age a tu? "
+age = gets.chomp
+print "Combien mesures tu en cm? "
+height = gets.chomp
+print "Quel est ton poids en kg? "
+weight = gets.chomp
+
+puts "Donc, tu as #{age} ans, tu fais #{height}cm et tu pèses #{weight}kg." 
